2df0c7ff1c2c695a43cd7acd2d82534d19f9fefe
HamedBahram <64147793+HamedBahram@users.noreply.github.com>
2023-04-14 13:16:52 -0400

    Add callbackUrl

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -6,13 +6,13 @@ const Header = () => {
     <header className='flex h-24 flex-col justify-center bg-stone-100'>
       <nav className='container'>
         <ul className='flex items-center justify-between gap-8 font-medium tracking-wider text-stone-500'>
-          <li className='text-sm uppercase'>
+          <li className='text-sm'>
             <Link href='/'>Home</Link>
           </li>
-          <li className='text-sm uppercase'>
+          <li className='text-sm'>
             <Link href='/protected/server'>Protected (server)</Link>
           </li>
-          <li className='text-sm uppercase'>
+          <li className='text-sm'>
             <Link href='/protected/client'>Protected (client)</Link>
           </li>
           <li>
--- app/protected/client/page.jsx
+++ app/protected/client/page.jsx
@@ -1,9 +1,15 @@
 'use client'
 
 import { useSession } from 'next-auth/react'
+import { redirect } from 'next/navigation'
 
 const ClientProtectPage = () => {
-  const { data: session } = useSession({ required: true })
+  const { data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
+      redirect('/signin?callbackUrl=/protected/client')
+    }
+  })
 
   return (
     <section className='py-24'>
